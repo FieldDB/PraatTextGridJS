7d099f0842af5ab039506f2e2c07cb9ad47b783a
SymonJoryStevens-Guille <symon.jsg@gmail.com>
2014-05-27 20:48:43 -0400

    This is a dummy test

---
+++ test/audio-intervals-spec.js
@@ -0,0 +1,78 @@
+'use strict';
+
+var textgrid = require("../lib/textgrid.js").TextGrid;
+var fs = require("fs");
+/*
+  ======== A Handy Little Jasmine Reference ========
+https://github.com/pivotal/jasmine/wiki/Matchers
+
+  Spec matchers:
+    expect(x).toEqual(y); compares objects or primitives x and y and passes if they are equivalent
+    expect(x).toBe(y); compares objects or primitives x and y and passes if they are the same object
+    expect(x).toMatch(pattern); compares x to string or regular expression pattern and passes if they match
+    expect(x).toBeDefined(); passes if x is not undefined
+    expect(x).toBeUndefined(); passes if x is undefined
+    expect(x).toBeNull(); passes if x is null
+    expect(x).toBeTruthy(); passes if x evaluates to true
+    expect(x).toBeFalsy(); passes if x evaluates to false
+    expect(x).toContain(y); passes if array or string x contains y
+    expect(x).toBeLessThan(y); passes if x is less than y
+    expect(x).toBeGreaterThan(y); passes if x is greater than y
+    expect(function(){fn();}).toThrow(e); passes if function fn throws exception e when executed
+
+    Every matcher"s criteria can be inverted by prepending .not:
+    expect(x).not.toEqual(y); compares objects or primitives x and y and passes if they are not equivalent
+
+    Custom matchers help to document the intent of your specs, and can help to remove code duplication in your specs.
+    beforeEach(function() {
+      this.addMatchers({
+
+        toBeLessThan: function(expected) {
+          var actual = this.actual;
+          var notText = this.isNot ? " not" : ";
+
+          this.message = function () {
+            return "Expected " + actual + notText + " to be less than " + expected;
+          }
+
+          return actual < expected;
+        }
+
+      });
+    });
+
+*/
+//Dummy test, does nothing. 
+("lib/textgrid", function() {
+
+
+  it("should convert sampleUtterancesTextGrid textgrid into json", function() {
+    var sampleUtterancesTextGrid = fs.readFileSync(__dirname + "/../data/sampleUtterances.TextGrid", {
+      encoding: "UTF-8"
+    });
+    var json = textgrid.textgridToJSON(sampleUtterancesTextGrid);
+    console.log(JSON.stringify(json, null, 2));
+    expect(typeof json).toBe("object");
+    for (var itemIndex = 0; itemIndex < json.items.length; itemIndex++) {
+      console.log("Sanity check for tier " + itemIndex);
+      if (json.items[itemIndex].points) {
+        expect(json.items[itemIndex].points.length).toEqual(json.items[itemIndex].points_size);
+      }
+      if (json.items[itemIndex].intervals) {
+        expect(json.items[itemIndex].intervals.length).toEqual(json.items[itemIndex].intervals_size);
+      }
+    }
+  }); // Symon: This tests json generation by ensuring that the items in the json object = the number of items in the textgrid file. 
+  
+
+  xit("should create one interval if there were no intervals in the TextGrid", function() {
+    var sampleEmptyTextGrid = fs.readFileSync(__dirname + "/../data/sampleEmpty.TextGrid", {
+      encoding: "UTF-8"
+    });
+    var json = textgrid.textgridToIGT(sampleEmptyTextGrid);
+    // console.log(JSON.stringify(json, null, 2));
+    expect(typeof json).toBe("object");
+    console.log(json.items.length);
+    expect(json.items.length).toEqual(1);
+  });
+});
